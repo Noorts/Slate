4e75caf6316dde3cae9b8abd5886aa33221613fb
Noorts <50550612+Noorts@users.noreply.github.com>
2021-06-18 11:13:31 +0200

    Cleanup content blocks service

--- frontend/src/app/services/content-blocks.service.ts
+++ frontend/src/app/services/content-blocks.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@angular/core';
-import { InfoCard } from 'src/app/models/InfoCard';
-import { ProjectCard } from '@models/ProjectCard';
 import { ContentBlock } from 'src/app/models/ContentBlock';
-import mockData from 'src/app/data/contentblocksmock.json';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Card } from '@models/Card';
 import { environment } from '@env/environment';
 
 @Injectable({
@@ -14,45 +10,22 @@ import { environment } from '@env/environment';
 class ContentBlocksService {
   public data: ContentBlock[] = [];
 
-  constructor(private httpClient: HttpClient) {
-    this.load();
-  } /* Hook this up to a database later on. */
+  constructor(private httpClient: HttpClient) { }
 
-  load(): void {
-    for (const contentblock of mockData) {
-      const listOfInfoCards: Card[] = [];
-      const contentType = contentblock.contentType;
-
-      for (const currentCard of contentblock.cards) {
-        const curObj = Object.assign(contentType === 'info-card' ? new InfoCard() : new ProjectCard(), currentCard);
-        listOfInfoCards.push(curObj);
-      }
-      this.data.push(Object.assign(new ContentBlock(), {
-        blockHeader: contentblock.blockHeader, contentType: contentblock.contentType, cards: listOfInfoCards
-      }));
-    }
-  }
-
-  getAllActivities(): Observable<ContentBlock[]> {
-    return new Observable<ContentBlock[]>(observer => {
-      observer.next(this.data);
-    });
+  private _httpGetLandingAreaInfo(): Observable<any> {
+    return this.httpClient.get<any>(`${environment.strapiApiUrl}/landing-area`);
   }
 
   private _httpGetContentBlocks(): Observable<any> {
     return this.httpClient.get<any>(`${environment.strapiApiUrl}/content-blocks`);
   }
 
-  getAllContentBlocks(): Observable<any> {
-    return this._httpGetContentBlocks();
-  }
-
-  private _httpGetLandingAreaInfo(): Observable<any> {
-    return this.httpClient.get<any>(`${environment.strapiApiUrl}/landing-area`);
-  }
-
   getLandingAreaInfo(): Observable<any> {
     return this._httpGetLandingAreaInfo();
+  }
+
+  getAllContentBlocks(): Observable<any> {
+    return this._httpGetContentBlocks();
   }
 }
 
