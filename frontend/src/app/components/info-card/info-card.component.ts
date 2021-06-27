5ebfebb7372f73da4b6cb15377945691c717de14
Noorts <50550612+Noorts@users.noreply.github.com>
2021-06-27 20:10:31 +0200

    Add read more functionality to info-card

--- frontend/src/app/components/info-card/info-card.component.ts
+++ frontend/src/app/components/info-card/info-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { InfoCard } from 'src/app/models/InfoCard';
 
 @Component({
@@ -6,9 +6,29 @@ import { InfoCard } from 'src/app/models/InfoCard';
   templateUrl: './info-card.component.html',
   styleUrls: ['./info-card.component.scss']
 })
-export class InfoCardComponent {
+export class InfoCardComponent implements AfterViewInit {
 
   @Input() model?: InfoCard = new InfoCard();
+  @ViewChild('infoCard') infoCardElement: ElementRef;
+  @ViewChild('description') descriptionElement: ElementRef;
+  @ViewChild('readMoreContainer') readMoreContainerElement: ElementRef;
 
   constructor() { }
+
+  ngAfterViewInit(): void {
+    if (!this.isDescriptionClamped()) {
+      this.readMoreContainerElement.nativeElement.classList.add('hidden');
+    }
+  }
+
+  isDescriptionClamped(): boolean {
+    const el = this.descriptionElement.nativeElement;
+    return el.scrollHeight > el.clientHeight;
+  }
+
+  revealFullDescription(): void {
+    this.descriptionElement.nativeElement.classList.add('revealed');
+    this.infoCardElement.nativeElement.classList.add('revealed');
+    this.readMoreContainerElement.nativeElement.classList.add('hidden');
+  }
 }
