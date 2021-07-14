fc7159f2407d2180d7b4bf363d64cdb4b94a7354
Noorts <50550612+Noorts@users.noreply.github.com>
2021-07-14 10:51:24 +0200

    Add image to infoCard

--- frontend/src/app/models/InfoCard.ts
+++ frontend/src/app/models/InfoCard.ts
@@ -6,14 +6,16 @@ class InfoCard implements Card {
     public organisation: string;
     public dateRange: string;
     public description: string;
+    public cardImageUrl: string;
 
     constructor(statusContent: string = '', header: string = '', organisation: string = '',
-                dateRange: string = '', description: string = '') {
+                dateRange: string = '', description: string = '', cardImageUrl: string = '') {
             this.statusContent = statusContent;
             this.header = header;
             this.organisation = organisation;
             this.dateRange = dateRange;
             this.description = description;
+            this.cardImageUrl = cardImageUrl;
         }
 }
 
